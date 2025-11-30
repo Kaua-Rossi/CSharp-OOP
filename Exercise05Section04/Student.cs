8d0816ce6d72464a52939eac6322a7832b26f6f5
K_Rossi <kauarossipersonal@gmail.com>
2025-11-30 16:21:16 -0300

    refactoring: remove unused namespaces

--- Exercise05Section04/Student.cs
+++ Exercise05Section04/Student.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Exercise05Section04
+﻿namespace Exercise05Section04
 {
     internal class Student
     {
