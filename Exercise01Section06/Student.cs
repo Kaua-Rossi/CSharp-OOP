3f9a44957edd869d14318502bdc4f564ffeab8d0
Kaua-Rossi <kauarossipersonal@gmail.com>
2026-02-13 15:39:20 -0300

    refactor: removes not used namespaces

--- Exercise01Section06/Student.cs
+++ Exercise01Section06/Student.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Exercise01Section06
+﻿namespace Exercise01Section06
 {
     internal class Student
     {
--- Exercise02Section06/Employee.cs
+++ Exercise02Section06/Employee.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Exercise02Section06
+﻿namespace Exercise02Section06
 {
     internal class Employee
     {
